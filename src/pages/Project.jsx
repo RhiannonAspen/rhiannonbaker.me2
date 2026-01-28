3960aeae0fbab47a716225d483af5b13069eec1f
Rhiannon Baker <rhiannonaspen.b@gmail.com>
2026-01-28 11:18:56 -0600

    Updated certifications popups for ALL certifications and added subtitle for user experience

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -54,6 +54,17 @@ function Login() {
     dataArea.innerText = "\n Understanding what IT is and how it supports businesses\n Understanding how operating systems work and how to install / uninstall software \n Recognizing common security threats and common cyber security practices \n Basic problem-solving approach to IT issues \n Understanding documentation and following procedures";
   }
   
+const clickAzureFund = () => {
+    const dataDisplay = document.getElementById("display-data");
+
+    dataDisplay.style.display = "flex";
+
+    const dataHeading = document.getElementById("data-heading");
+    const dataArea = document.getElementById("data-text");
+    dataHeading.innerText = "AZ-900 Microsoft Azure Fundamentals"
+    dataArea.innerText = "\n Solid building blocks of Cloud Computing and Cloud Models\n Ability to explain and recommend Core Azure Services\n Azure Security, Compliance and Trust concepts\n Azure Pricing and Cost Management Techniques";
+  }
+
 const clickAPlus = () => {
     const dataDisplay = document.getElementById("display-data");
 
@@ -63,6 +74,28 @@ const clickAPlus = () => {
     const dataArea = document.getElementById("data-text");
     dataHeading.innerText = "CompTIA A+"
     dataArea.innerText = "\n Gained practical and hands-on skills understanding Computer Hardware, Operating System Management, Networking, Secuirty, and Troubleshoooting Skills \n Diagnosing hardware and software failures and replacing components when needed \n Installing, Configuring , and maintaining Operating Systems such as Windows 10 and 11, Linux Distibutions (CentOS, RHEL 8), and MacOS \n User accounts, permissions, and system settings \n Command-line tools \n System recovery, backups, and restore options \n Malware removal, Securing devices (firewalls, antivirus, encryption), User authentication and access control";
+  }
+
+  const clickAvaya = () => {
+    const dataDisplay = document.getElementById("display-data");
+
+    dataDisplay.style.display = "flex";
+
+    const dataHeading = document.getElementById("data-heading");
+    const dataArea = document.getElementById("data-text");
+    dataHeading.innerText = "Avaya Aura Communication Manager Specialized Administrator"
+    dataArea.innerText = "\n Enterprise Telephony Fundamentals such as PBX concepts, call flow logic, extensions, stations, trunks, and hunt groups\n Voice Networking Concepts such as SIP and H.323 fundamentals, VoIP call signalling, and Quality of Service concepts\n Integration of Enterprise level Features specifically utilizing the Avaya Series of Products";
+  }
+
+  const clickToscaAs1 = () => {
+    const dataDisplay = document.getElementById("display-data");
+
+    dataDisplay.style.display = "flex";
+
+    const dataHeading = document.getElementById("data-heading");
+    const dataArea = document.getElementById("data-text");
+    dataHeading.innerText = "Tricentis Tosca Automation Specialist 1"
+    dataArea.innerText = "\n Model-based Software Testing Automation Foundations\n Software Quality Assurance Concepts such as Defect lifecycle, Test coverage, and risk-based testing\n CI/CD & DevOps Awareness from a Software perspective";
   }
 
   return (
@@ -82,19 +115,20 @@ const clickAPlus = () => {
 
       <div className="featured">
         Accreddited Certifications
+        <p>Click on the icons below to get more details about the certificate</p>
         <div className="certifications">
           <img src={cvtech} onClick={clickCVTECH} alt="Technical Diploma in Computer Programming and Repair" />
           <img src={associates} onClick={clickAssociates} alt="Associates of Science in Data Analytics" />
           <img src={itf} onClick={clickITF} alt="CompTIA ITF+ Ceritifcation" />
           <img src={aplus} onClick={clickAPlus} alt="CompTIA A+ Certification" />
-          <img src={azurefund} alt="Microsoft Azure Fundamentals AZ-900" />
-          <img src={avaya} alt="Avaya Aura Communication Manager Administrator Specialized" />
-          <img src={toscaas1} alt="Tricentis Tosca Automation Specialist 1" />
+          <img src={azurefund} onClick={clickAzureFund} alt="Microsoft Azure Fundamentals AZ-900" />
+          <img src={avaya} onClick={clickAvaya} alt="Avaya Aura Communication Manager Administrator Specialized" />
+          <img src={toscaas1} onClick={clickToscaAs1} alt="Tricentis Tosca Automation Specialist 1" />
         </div>
       </div>
       <div id="display-data">
         <h2 id='data-heading'></h2>
-        <p id='data-text'>Click on one of the icons above to get more details about this certificate</p>
+        <p id='data-text'></p>
         <button onClick={clickClose} id='close-data'>Close</button>
       </div>
       <Footer />
